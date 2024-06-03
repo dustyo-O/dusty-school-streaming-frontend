f667cb0d6b80741426b09b51c323ae62bc3b02af
Alexander Shleyko <a.shleyko@yandex.ru>
2024-06-03 20:50:58 +0200

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useRef } from 'react';
+
 import './App.css';
 
 function App() {
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const streamRef = useRef<Promise<MediaStream>>();
+  const recorderRef = useRef<MediaRecorder>();
+
+  const handleStart = () => {
+    streamRef.current = navigator.mediaDevices.getUserMedia({ video: true });
+
+    streamRef.current.then((stream) => {
+      if (!videoRef.current) {
+        return;
+      }
+      videoRef.current.srcObject = stream;
+      videoRef.current.onloadedmetadata = () => {
+        if (!videoRef.current) {
+          return;
+        }
+
+        videoRef.current.play();
+      };
+
+      recorderRef.current = new MediaRecorder(stream, {
+        mimeType: 'video/webm; codecs=vp9',
+      });
+
+      recorderRef.current.ondataavailable = (event) => {
+        console.log('Записался кусок');
+
+        const file = new File([event.data], 'video.webm');
+        const data = new FormData();
+
+        data.append('id', 'test');
+        data.append('video', file);
+
+        fetch('http://localhost:3000/files/_upload', {
+          method: 'POST',
+          body: data
+        });
+      };
+
+      recorderRef.current.start(10000);
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <video ref={videoRef}></video>
+      <button onClick={handleStart}>Start</button>
     </div>
   );
 }
